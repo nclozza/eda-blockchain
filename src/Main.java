98ba7804183b2fd5f2ceeffb61ace6bea687f969
tdallas <tdallas@itba.edu.ar>
2017-10-03 12:49:59 -0300

    Added pre implementation of AVL tree (NodeClass and some methods)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    AVLTree<Integer> tree = new AVLTree<>(5);
+	    tree.addNode(6);
+	    tree.addNode(7);
+	    tree.addNode(8);
+	    tree.addNode(9);
+	    tree.addNode(10);
+	    tree.addLeft(4);
+	    tree.addLeft(3);
+	    tree.addLeft(2);
+		System.out.println("Max level: " + tree.getMaxLevel());
+	    System.out.println(tree.getHeight());
+	    System.out.println(tree.isBalanced());
     }
 }
