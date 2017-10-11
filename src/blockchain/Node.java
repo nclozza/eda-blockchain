95b20aac1a13091a3d4235a947ae474c1c0d0a15
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-11 20:44:32 -0300

    Improved style, added javadoc and added equals and hashcode methods.

--- src/blockchain/Node.java
+++ src/blockchain/Node.java
@@ -1,26 +1,31 @@
 package blockchain;
 
+/**
+ * The Node class is a representation of a node that has data stored in it and a reference to its right and left
+ * children. There is also the height variable to store the height of the node in a tree instead of travelling through
+ * the tree to obtain it. This class is the one used for the AVLTree implementation.
+ */
 public class Node<T> {
     private T data;
     private int height;
     private Node<T> right = null;
     private Node<T> left = null;
 
+    /**
+     * The constructor of a node cannot be called without specifying what data the node will store.
+     * @param data  The data to be stored in the node.
+     */
     public Node(T data) {
         this.data = data;
         this.height = 0;
-    }
-
-    public int getHeight() {
-        return this != null?this.height:0;
     }
 
     public T getData() {
         return this.data;
     }
 
-    public void setData(T data) {
-        this.data = data;
+    public int getHeight() {
+        return this.height;
     }
 
     public Node<T> getRight() {
@@ -31,14 +36,44 @@ public class Node<T> {
         return this.left;
     }
 
-    public void setRight(Node<T> node) {this.right = node;
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setRight(Node<T> node) {
+        this.right = node;
     }
 
     public void setLeft(Node<T> node) {
         this.left = node;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    /**
+     * A Node instance will be equal to another if their data stored is the same.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!obj.getClass().equals(Node.class)) {
+            return false;
+        }
+
+        if (obj.hashCode() != this.hashCode()) {
+            return false;
+        }
+
+        return ((Node)obj).getData().equals(this.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return 53 * data.toString().hashCode();
     }
 }
