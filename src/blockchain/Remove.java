804499fd66639964b369a79051bcc6c8b74f9aa2
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-12 20:37:26 -0300

    Remove javadoc added

--- src/blockchain/Remove.java
+++ src/blockchain/Remove.java
@@ -1,12 +1,14 @@
 package blockchain;
 
+/**
+ * The Remove class represents the operation of using the remove method upon the AVL tree.
+ */
 public class Remove<T> extends Operation {
+    public Remove(T data, boolean status) {
+        super("Remove", data, status);
+    }
 
-  public Remove(T data, boolean status) {
-    super("Remove", data, status);
-  }
-
-  public String oprationClass() {
-    return "Remove";
-  }
+    public String operationClass() {
+        return "Remove";
+    }
 }
