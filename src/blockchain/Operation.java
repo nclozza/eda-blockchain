cef0cf47ac189ba3652cb8d25d4c78b7c53e9235
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-12 20:09:40 -0300

    Added javadoc to the operation class

--- src/blockchain/Operation.java
+++ src/blockchain/Operation.java
@@ -1,45 +1,53 @@
 package blockchain;
 
+/**
+ * The Operation class represents an operation made upon an AVL tree consisting of the operation's name, the data that
+ * is passed to the operation method and the status of the operation which indicates if said operation was successfull
+ * or not.
+ */
 public abstract class Operation<T> {
+    private String name;
+    private T data;
+    /**
+     * The status indicates if the operation was applied successfully (true) or unsuccessfully (false).
+     */
+    private boolean status;
 
-  private String name;
-  private T data;
-  private boolean status;
+    public Operation(String name, T data, boolean status) {
+        this.name = name;
+        this.data = data;
+        this.status = status;
+    }
 
-  public Operation(String name, T data, boolean status) {
-    this.name = name;
-    this.data = data;
-    this.status = status;
-  }
+    public String getName() {
+        return this.name;
+    }
 
-  public String getName() {
-    return this.name;
-  }
+    public T getData() {
+        return data;
+    }
 
-  public T getData() {
-    return data;
-  }
+    public boolean getStatus() {
+        return status;
+    }
 
-  public boolean isStatus() {
-    return status;
-  }
+    public String toString() {
+        return "The " + this.getName() + " operation was applied with the " + this.getData()
+                + " node passsed as an argument " + (this.getStatus() ? "successfully." : "unsuccessfully.");
+    }
 
-  public String toString() {
-    return "Se aplico la operacion " + this.getName() + " al nodo " + this.getData() + ". Estado de la operacion: " + this.isStatus();
-  }
+    public String toStringForHash() {
+        return this.getName() + this.getData() + this.getStatus();
+    }
 
-  public String toStringForHash() {
-    return this.getName() + this.getData() + this.isStatus();
-  }
+    /**
+     * IMPORTANT: This method is only available so we can simulate an unwanted data manipulation
+     */
+    public void modifyOperation(T data) {
+        this.data = data;
+    }
 
-  /**
-   * IMPORTANT: This method is only available so we can simulate an unwanted data manipulation
-   */
-  public void modifyOperation(T data) {
-    this.data = data;
-  }
-
-  public String oprationClass() {
-    return "";
-  }
+    public String operationClass() {
+        return "";
+    }
 }
