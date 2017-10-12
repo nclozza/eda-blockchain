ffb2ba286533057ac26c7c70a61cae332688559f
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-12 20:32:49 -0300

    Data javadoc added

--- src/blockchain/Data.java
+++ src/blockchain/Data.java
@@ -1,24 +1,31 @@
 package blockchain;
 
+/**
+ * The Data class represents the data stored in each block. The data consists of an operation and the hash reference
+ * to the AVL tree stage after said operation was applied. The actual AVL tree stage is stored in a remote server to
+ * reduce the storage of each user of the blockchain.
+ */
 public class Data<T> {
+    private Operation<T> operation;
+    /**
+     * The reference to the AVL tree stage after the operation was applied to it.
+     */
+    private String avlTreeHash;
 
-  private Operation<T> operation;
-  private String avlTreeHash;
+    public Data(Operation<T> operation, String avlTreeHash) {
+        this.operation = operation;
+        this.avlTreeHash = avlTreeHash;
+    }
 
-  public Data(Operation<T> operation, String avlTreeHash) {
-    this.operation = operation;
-    this.avlTreeHash = avlTreeHash;
-  }
+    public Operation<T> getOperation() {
+        return operation;
+    }
 
-  public Operation<T> getOperation() {
-    return operation;
-  }
+    public String getAvlTreeHash() {
+        return avlTreeHash;
+    }
 
-  public String getAvlTreeHash() {
-    return avlTreeHash;
-  }
-
-  public String toStringForHash() {
-    return this.getOperation().toStringForHash() + this.getAvlTreeHash();
-  }
+    public String toStringForHash() {
+        return this.getOperation().toStringForHash() + this.getAvlTreeHash();
+    }
 }
