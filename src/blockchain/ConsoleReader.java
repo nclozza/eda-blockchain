ef50b4a548df73582ca90044a4fb6dc4aa4b8096
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-12 20:39:47 -0300

    ConsoleReader javadoc added

--- src/blockchain/ConsoleReader.java
+++ src/blockchain/ConsoleReader.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * The ConsoleReader class is used to read user input from the console to act upon the AVL tree.
+ */
 public class ConsoleReader {
-
     public static String readingFromConsole() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         return br.readLine();
     }
 }
