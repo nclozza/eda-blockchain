839f11522904d30c1f28255fe9d9e7e866265191
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-13 00:48:27 -0300

    DuplicateNodeInsertException javadoc added

--- src/blockchain/DuplicateNodeInsertException.java
+++ src/blockchain/DuplicateNodeInsertException.java
@@ -1,5 +1,9 @@
 package blockchain;
 
+/**
+ * The DuplicateNodeInsertException is thrown whenever a user tries to add content to the AVL tree that is already
+ * contained by a node.
+ */
 public class DuplicateNodeInsertException extends Exception {
     public DuplicateNodeInsertException() {}
 
